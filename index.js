bb8a44ad98761cdcb03d44b8db330a9a2f320915
pollyp0 <9033303010@mail.ru>
2024-06-19 13:23:50 +0300

    added comic

--- index.js
+++
@@ -1,8 +0,0 @@
-let blueBlock = document.getElementById("blueBlock");
-let preview = document.getElementById("photo")
-
-
-setInterval(() => {
-    blueBlock.style.width = preview.clientWidth + "px";
-    blueBlock.style.height = preview.clientHeight + "px";
-}, 500)
